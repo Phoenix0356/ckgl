2174ff7f2de50af3e10af5aeaa90cb2696e03fa5
Phoenix0356 <1242780667@qq.com>
2023-07-03 21:54:23 +0800

    initial commit

--- demo/src/main/java/com/ckgl/service/Impl/UserServiceImpl.java
+++ demo/src/main/java/com/ckgl/service/Impl/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.ckgl.entity.User;
 import com.ckgl.mapper.UserMapper;
 import com.ckgl.service.UserService;
 import org.springframework.stereotype.Service;
-
+//test
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
